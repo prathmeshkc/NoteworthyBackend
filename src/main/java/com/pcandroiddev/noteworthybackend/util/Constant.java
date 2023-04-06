6a5349e41f064311c101cc819bb18f82eb46cd6d
Prathmesh Chaudhari <76175584+prathmeshkc@users.noreply.github.com>
2023-04-06 15:14:33 -0400

    Update Constant.java

--- src/main/java/com/pcandroiddev/noteworthybackend/util/Constant.java
+++ src/main/java/com/pcandroiddev/noteworthybackend/util/Constant.java
@@ -3,7 +3,7 @@ package com.pcandroiddev.noteworthybackend.util;
 import org.springframework.context.annotation.Bean;
 
 public class Constant {
-      public static String CLOUD_NAME = "dcqa6vckq";
-      public static String API_KEY = "968496778275963";
-      public static String API_SECRET = "IS1ns46iUHrfW2QQ7QC70t1WM-k";
+      public static String CLOUD_NAME = "YOUR_CLOUD_NAME";
+      public static String API_KEY = "YOUR API KEY";
+      public static String API_SECRET = "YOUR API SRCRET";
 }
