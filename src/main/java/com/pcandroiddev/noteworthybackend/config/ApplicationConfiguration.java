e470a1083d2b247bcb1785a9b4071b9c4c41accd
prathmeshkc <prathmeshkc2001@gmail.com>
2023-04-06 22:52:52 -0400

    -configured railway datasource

--- src/main/java/com/pcandroiddev/noteworthybackend/config/ApplicationConfiguration.java
+++ src/main/java/com/pcandroiddev/noteworthybackend/config/ApplicationConfiguration.java
@@ -2,7 +2,6 @@ package com.pcandroiddev.noteworthybackend.config;
 
 import com.cloudinary.Cloudinary;
 import com.pcandroiddev.noteworthybackend.dao.user.UserDao;
-import com.pcandroiddev.noteworthybackend.util.Constant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
